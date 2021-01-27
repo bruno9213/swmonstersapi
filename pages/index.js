2553b1463aef95b1ae66608a84c9ae72593a70fe
b.ferreira <b.ferreira@ua.pt>
2021-01-27 18:15:17 +0000

    gg2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home() {  
   return (
     <div>
-      <h2>A minha pipoquinha é linda!</h2>
-      <p>Podes clicar <a href="/api/sw">aqui</a> para veres a minha api xD</p>
+      <h2>Parabéns! Você é bronze!</h2>
+      <p>Podes clicar <a href="/api/sw">aqui</a> para veres a minha api do SW xD</p>
+      <div id="emitter"></div>
     </div>
   )
 }
